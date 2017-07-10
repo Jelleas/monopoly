4f08d3159175aff486bdd282395255ee7ff89e62
Jelleas <jelle.van.assema@gmail.com>
2017-07-10 17:01:20 +0200

    persistent board

--- monopoly.py
+++ monopoly.py
@@ -1,83 +1,36 @@
-import multiprocessing
 import monopolyVisualisation
-		
-class Monopoly(object):
-	def __init__(self):
-		self.board = Board()
-		self.pawn = Pawn()
-		self._pawnLocation = 0
-		
-		# start visualisation
-		self._boardQueue = multiprocessing.Queue()		
-		p = multiprocessing.Process(target=monopolyVisualisation.visualize, args=(self._boardQueue,), name="monopolyVisualisation")
-		p.start()
-	
-	def movePawn(self, distance):
-		self._pawnLocation = (self.pawnLocation + distance) % len(self.board.cells)
-		self.pawn.placeAt(self.board.cells[self._pawnLocation])
-
-	def draw(self):
-		self._boardQueue.put(self.board)
+import monopolyData
 
 class Pawn(object):
 	def __init__(self, cell):
-		self.location = cell
+		self.placeAt(cell)
 
 	def placeAt(self, cell):
-		self.location = cell
+		self.cell = cell
 
 class Board(object):
 	def __init__(self):
-		names = ["start",\
-		 		 "dorpstraat",\
-		 		 "algemeen fonds",\
-		 		 "brink",\
-		 		 "inkomstenbelasting",\
-		 		 "station zuid",\
-		 		 "steenstraat",\
-		 		 "kans",\
-		 		 "ketelstraat",\
-		 		 "velperplein",\
-		 		 "gevangenis",\
-		 		 "barteljorisstraat",\
-		 		 "elecriciteitsbedrijf",\
-		 		 "zijlweg",\
-		 		 "houtstraat",\
-		 		 "station west",\
-		 		 "neude",\
-		 		 "algemeen fonds",\
-		 		 "bilstraat",\
-		 		 "vreeburg",\
-		 		 "vrij parkeren",\
-		 		 "a-kerkhof",\
-		 		 "kans",\
-		 		 "groote markt",\
-		 		 "heerestraat",\
-		 		 "station noord",\
-		 		 "spui",\
-		 		 "plein",\
-		 		 "waterleiding",\
-		 		 "lange poten",\
-		 		 "naar de gevangenis",\
-		 		 "hofplein",\
-		 		 "blaak",\
-		 		 "algemeen fonds",\
-		 		 "station oost",\
-		 		 "kans",\
-		 		 "leidschestraat",\
-		 		 "extra belasting",\
-		 		 "klaverstraat"]
-		values = [0, 60, 0, 60, 0, 200, 100, 0, 100, 120,\
-				  0, 140, 120, 140, 160, 200, 180, 0, 180, 200,\
-				  0, 220, 0, 220, 240, 200, 260, 260, 150, 280,\
-				  0, 300, 300, 0, 320, 200, 0, 380, 0, 400]
-		self.cells = [Cell(name, value) for name, value in zip(names,values)]
+		self.cells = [Cell(name, value) for name, value in zip(monopolyData.names, monopolyData.values)]
+		self._pawnLocation = 0
+		self.pawn = Pawn(self.cells[self._pawnLocation])
+		
+	def move(self, distance):
+		self._pawnLocation = (self._pawnLocation + distance) % len(self.cells)
+		self.pawn.placeAt(self.cells[self._pawnLocation])
 
 class Cell(object):
 	def __init__(self, name, value):
 		self.name = name
 		self.value = value
 
+def draw(board):
+	monopolyVisualisation.draw(board)
+
 if __name__ == "__main__":
-	board = Monopoly()
-	board.draw()
+	import time
+	board = Board()
+
+	for i in range(100):
+		draw(board)
+		time.sleep(1)
+		board.move(1)
--- monopolyVisualisation.py
+++ monopolyVisualisation.py
@@ -5,7 +5,7 @@ import multiprocessing
 class MonopolyVisualisation(object):
 	def __init__(self, boardQueue):
 		self._boardQueue = boardQueue
-		self._updateTime = 1000
+		self._updateTime = 100
 		self._width = 450
 		self._height = 450
 		self._padding = 0.042 * self._width
@@ -24,30 +24,46 @@ class MonopolyVisualisation(object):
 
 	def run(self):
 		self._tk = Tkinter.Tk()
-		
 		self._canvas = Tkinter.Canvas(self._tk, height=self._height, width=self._width)
 		self._canvas.pack()
 		backgroundImage = Tkinter.PhotoImage(file="monopoly_background.gif")
 		self._canvas.create_image(0, 0, image = backgroundImage, anchor = "nw")
-	
+		self._canvas.background = backgroundImage
+		
 		self._tk.after(0, self._update)
 		self._tk.mainloop()	
 
+	def _reset(self):
+		self._canvas.delete("pawn")
+
 	def _update(self):
 		if not self._boardQueue.empty():
-			#self._reset()	
-			board = self._boardQueue.get()
-
-			for i in range(len(self._cellPositions)):
-				x0, y0 = self._cellPositions[i]
-				x1 = x0 + self._cellWidth
-				y1 = y0 + self._cellHeight
-				self._canvas.create_rectangle(x0, y0, x1, y1, fill="red")
+			self._reset()
 			
+			board = self._boardQueue.get()
+			
+			x0, y0 = self._cellPositions[board._pawnLocation]
+			carImage = Tkinter.PhotoImage(file="car.gif")
+			self._canvas.create_image(x0 + 18, y0 + 18, image=carImage, tag="pawn")
+			self._canvas.pawn = carImage
+
 		self._tk.after(self._updateTime, self._update)
 		
+_boardQueue = None
+_visualisationProcess = None
+def draw(board):
+	global _boardQueue
+	global _visualisationProcess
+	
+	# if visualisation has not started, start
+	if not _boardQueue or not _visualisationProcess or not _visualisationProcess.is_alive():
+		_boardQueue = multiprocessing.Queue()
+		_visualisationProcess = multiprocessing.Process(target=_visualize, args=(_boardQueue,), name="monopolyVisualisation")
+		_visualisationProcess.start()
 
-def visualize(boardQueue):
+	# add board to the "to be drawed" queue
+	_boardQueue.put(board)
+
+def _visualize(boardQueue):
 	visualisation = MonopolyVisualisation(boardQueue)
 	visualisation.run()
-		
